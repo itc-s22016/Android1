95ea99152a25c494db9cdc3456f99d88cac51206
s22016 <s22016@std.it-college.ac.jp>
2023-08-15 13:58:15 +0900

    9.2 fin

--- FragmentSample/app/src/main/java/jp/ac/it_college/std/s22016/fragmentsample/MainActivity.kt
+++ FragmentSample/app/src/main/java/jp/ac/it_college/std/s22016/fragmentsample/MainActivity.kt
@@ -2,8 +2,10 @@ package jp.ac.it_college.std.s22016.fragmentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s22016.fragmentsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
